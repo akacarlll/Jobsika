913272ea90550098e4d3f757ee34a755aaf18503
aka.carlll <carlfogue193@gmail.com>
2025-09-24 01:54:25 +0200

    🐛 Add whitenoise to display css on production

--- jobsika/settings.py
+++ jobsika/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
 ]
 
 ROOT_URLCONF = "jobsika.urls"
@@ -145,8 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'statifiles')
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "jobs_engine/static/",
     BASE_DIR / "home_page/static/"
