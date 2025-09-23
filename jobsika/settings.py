3edbc80fbe53b11a83b0595ca31514182fd7ed53
aka.carlll <carlfogue193@gmail.com>
2025-09-24 01:17:43 +0200

    🐛 Static root not set error

--- jobsika/settings.py
+++ jobsika/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "jobs_engine/static/",
